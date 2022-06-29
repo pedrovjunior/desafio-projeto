47d4ff8644d56e6cea58cc363a638c04910504f3
PedroValadresJunior[ <pvj.fagotista@protonmail.com>
2022-06-29 17:46:55 -0300

    Formulário Desafio Projeto- TransformaTec.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Users from './components/Users';
+import styles from './components/styles.modules.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+      
+    </div>
+  );
+}
+
+export default App;
